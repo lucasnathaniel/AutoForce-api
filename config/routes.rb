e005c75249a8e9d1e9baf06b35a2447430ab87cd
Lucas Nathaniel <lucasnataniel@hotmail.com>
2019-03-30 21:51:52 -0400

    Config Rack: Attack and Core. Add new routers for future implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :batches
+
+  resources :orders do
+    collection do
+      get '/search/:name', to: 'orders#search' 
+    end
+  end
+
+  resources :batches do
+    member do
+      get :summary
+    end
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
